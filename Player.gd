07a456d74d6be80641006c46420eee9b629abcd0
1r1di0us <57327873+1r1di0us@users.noreply.github.com>
2024-02-25 17:16:43 -0700

    replaced sprites and almost got AnimationPlayer working
    Need to replace PlayerSprite with AnimationPlayer and get that working but I can't figure it out for the life of me

--- Player.gd
+++ Player.gd
@@ -2,16 +2,19 @@ extends CharacterBody2D
 
 @export var SPEED = 300.0
 @export var JUMP_VELOCITY = -400.0
-var state = 0 #0 = normal, 1 = crouched
+var can_jump = true
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@export var gravity = 1500
 
 
 func _physics_process(delta):
 	
+	#TODO: replace stupid PlayerSprite with AnimationPlayer and figure out how that solves the crisis
+	#around the collision shapes and changing their sizes, because apparently its the solution
+	# https://www.reddit.com/r/godot/comments/11ps5nr/what_is_the_correct_way_to_change_the_collision/
+	
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right") # direction = -1, 0, or 1, I think
 	if direction:
 		velocity.x = direction * SPEED
@@ -21,19 +24,25 @@ func _physics_process(delta):
 	if is_on_floor():
 		if Input.is_action_just_pressed("jump"):
 			velocity.y = JUMP_VELOCITY
+			$PlayerCollision.shape.extents = Vector2(37, 48)
 			$PlayerSprite.animation = "jump"
 		elif Input.is_action_pressed("crouch"):
+			$PlayerCollision.shape.extents = Vector2(42, 24)
 			if velocity.x == 0:
 				$PlayerSprite.animation = "crouch"
 			else:
 				$PlayerSprite.animation = "crawl"
 		else:
 			if velocity.x == 0:
+				$PlayerCollision.shape.extents = Vector2(24, 33)
 				$PlayerSprite.animation = "idle"
 			else:
+				$PlayerCollision.shape.size = Vector2(42, 33)
 				$PlayerSprite.animation = "run"
 	else: # not on floor
 		# add gravity
+		#$PlayerCollision.shape.extents = Vector2(37, 48)
+		#$PlayerCollision.transform.position = Vector2(37, 48)
 		velocity.y += gravity * delta
 		if $PlayerSprite.animation == "jump" and $PlayerSprite.frame == 5:
 			$PlayerSprite.animation = "freefall"
