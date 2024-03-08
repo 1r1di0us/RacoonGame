9cfe900e0ad3088d5bbf01c6956c116da27ec9c7
1r1di0us <57327873+1r1di0us@users.noreply.github.com>
2024-03-08 16:02:22 -0700

    in the midst of making a big mistake?

--- scripts/raccoon.gd
+++ scripts/raccoon.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 400.0
-const JUMP_VELOCITY = -520.0
+const JUMP_VELOCITY = -720.0
 
 const ACCELERATION = 500
 const MAX_SPEED = 80
@@ -9,8 +9,8 @@ const FRICTION = 500
 
 @onready var animationPlayer = $AnimationPlayer
 
-var facing = false
-var stored_jump = false
+var facing = 0
+var prev_facing = 0
 
 #TODO: get AudioStreamPlayer on racoon.tscn and make jump sound work
 #@onready var jump_sound: AudioStreamPlayer = $JumpSound
@@ -24,37 +24,31 @@ func _physics_process(delta):
 	var direction = Input.get_axis("move_left", "move_right") # direction = -1, 0, or 1, I think
 	if direction != 0:
 		velocity.x = direction * SPEED
-		facing = (-direction+1)/2 #facing is never 0
+		prev_facing = facing
+		facing = (-direction+1)/2 #facing is 0 or 1
 	else: #reduce velocity by SPEED(300) every frame until velocity is 0
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	$AnimatedSprite2D.flip_h = facing
+	#$AnimatedSprite2D.flip_h = facing
 	if is_on_floor():
-		if Input.is_action_pressed("jump") && stored_jump: #pressing and holding jump again while falling 
-			stored_jump = false
+		if Input.is_action_just_pressed("jump") && !Input.is_action_pressed("crouch"): #can't jump while crouching
 			velocity.y = JUMP_VELOCITY
-			animationPlayer.play("jump")
-			$AnimatedSprite2D.position.y = $AnimatedSprite2D.scale.y * -45
+			#animationPlayer.play("jump")
+			#$AnimatedSprite2D.position.y = $AnimatedSprite2D.scale.y * -160
 			if !facing:
-				$AnimatedSprite2D.position.x = $AnimatedSprite2D.scale.x * -60
+				animationPlayer.play("jump")
+			#	$AnimatedSprite2D.position.x = $AnimatedSprite2D.scale.x * -60
 			else:
-				$AnimatedSprite2D.position.x = $AnimatedSprite2D.scale.x * 60
-		elif Input.is_action_just_pressed("jump") && !Input.is_action_pressed("crouch"): #can't jump while crouching
-			velocity.y = JUMP_VELOCITY
-			animationPlayer.play("jump")
-			$AnimatedSprite2D.position.y = $AnimatedSprite2D.scale.y * -45
-			if !facing:
-				$AnimatedSprite2D.position.x = $AnimatedSprite2D.scale.x * -60
-			else:
-				$AnimatedSprite2D.position.x = $AnimatedSprite2D.scale.x * 60
+				animationPlayer.play("jump_flip")
+			#	$AnimatedSprite2D.position.x = $AnimatedSprite2D.scale.x * 60
 			#jump_sound.play() #Play Jump Sound
 		elif Input.is_action_pressed("crouch"):
-			$AnimatedSprite2D.position.y = $AnimatedSprite2D.scale.y * 25
+			$AnimatedSprite2D.position.y = $AnimatedSprite2D.scale.y * -80
 			if velocity.x == 0:
 				animationPlayer.play("crouch")
 				if !facing:
-					$AnimatedSprite2D.position.x = $AnimatedSprite2D.scale.x * -55
+					$AnimatedSprite2D.position.x = $AnimatedSprite2D.scale.x * -60
 				else:
-					$AnimatedSprite2D.position.x = $AnimatedSprite2D.scale.x * 55
+					$AnimatedSprite2D.position.x = $AnimatedSprite2D.scale.x * 60
 			else:
 				animationPlayer.play("crawl")
 				if !facing:
@@ -64,35 +58,53 @@ func _physics_process(delta):
 		else:
 			if velocity.x == 0:
 				animationPlayer.play("idle")
-				$AnimatedSprite2D.position.y = $AnimatedSprite2D.scale.y * 0 #consistency is key
+				$AnimatedSprite2D.position.y = $AnimatedSprite2D.scale.y * -110
 				if !facing:
 					$AnimatedSprite2D.position.x = $AnimatedSprite2D.scale.x * -40
 				else:
 					$AnimatedSprite2D.position.x = $AnimatedSprite2D.scale.x * 40
 			else:
 				animationPlayer.play("run")
-				$AnimatedSprite2D.position.y = $AnimatedSprite2D.scale.y * 0
+				$AnimatedSprite2D.position.y = $AnimatedSprite2D.scale.y * -110
 				if !facing:
 					$AnimatedSprite2D.position.x = $AnimatedSprite2D.scale.x * -50
 				else:
 					$AnimatedSprite2D.position.x = $AnimatedSprite2D.scale.x * 50
 	#how to use walk animation:
 	#animationPlayer.play("walk)
-	#$AnimatedSprite2D.position.y = -3
+	#$AnimatedSprite2D.position.y = $AnimatedSprite2D.scale.y * -110
 	#if !facing:
-	#	$AnimatedSprite2D.position.x = -36
+	#	$AnimatedSprite2D.position.x = $AnimatedSprite2D.scale.x * -60
 	#else:
-	#	$AnimatedSprite2D.position.x = 36
+	#	$AnimatedSprite2D.position.x = $AnimatedSprite2D.scale.x * 60
 	else: # not on floor
 		velocity.y += gravity * delta
-		if !facing:
-			$AnimatedSprite2D.position.x = $AnimatedSprite2D.scale.x * -60
-		else:
-			$AnimatedSprite2D.position.x = $AnimatedSprite2D.scale.x * 60
+		#$AnimatedSprite2D.position.y = $AnimatedSprite2D.scale.y * -160
+		#if !facing:
+		#	$AnimatedSprite2D.position.x = $AnimatedSprite2D.scale.x * -60
+		#else:
+		#	$AnimatedSprite2D.position.x = $AnimatedSprite2D.scale.x * 60
 		if Input.is_action_just_released("jump") && velocity.y < 0:
 			velocity.y = 0
-		if Input.is_action_just_pressed("jump"): #pressing jump again mid-air. will only jump if you hold it
-			stored_jump = true
+		
+		if animationPlayer.current_animation == "jump" && animationPlayer.current_animation_position == 6:
+			animationPlayer.play("freefall")
+		elif animationPlayer.current_animation == "jump_flip" && animationPlayer.current_animation_position == 6:
+			animationPlayer.play("freefall_flip")
+		
+		if prev_facing != facing:
+			if animationPlayer.current_animation == "jump" || animationPlayer.current_animation == "jump_flip":
+				var temp = animationPlayer.current_animation_position
+				if facing:
+					animationPlayer.play("jump_flip", temp)
+				else:
+					animationPlayer.play("jump", temp)
+			elif animationPlayer.current_animation == "freefall" || animationPlayer.current_animation == "freefall_flip":
+				var temp = animationPlayer.current_animation_position
+				if facing:
+					animationPlayer.play("freefall_flip", temp)
+				else:
+					animationPlayer.play("freefall", temp)
 	move_and_slide()
 	
 	#var input_vector = Vector2.ZERO
