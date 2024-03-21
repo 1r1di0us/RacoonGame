3556b403cee90988d710df8eb6444aad35c257e1
bennilesuofc <117244844+bennilesuofc@users.noreply.github.com>
2024-03-20 23:50:01 -0600

    Level Timer
    -Added Level Timer
      -default is 120 sec
      -can change via each level's UIScreen
      -displays game over screen on timeout
    -Clock icon
      -shows level timer progress
      -spins at a rate of 1/level_timer
      -currently does a full rotation
    -level transition fixes
      -fixed UI showing above transition animations
      -fixed transition animations not hiding after playing
      -adjusted transition pause game timing
    -added enter key as a shortcut for "next" button in cutscenes, and for "main menu" button on pause screen and game over screen

--- scripts/cutscene.gd
+++ scripts/cutscene.gd
@@ -6,6 +6,10 @@ extends CanvasLayer
 
 func _on_button_pressed():
 	if target_level_path.is_empty(): return
-	await(LevelTransitions.play_exit_transition(play_exit_animation))
+	#await(LevelTransitions.play_exit_transition(play_exit_animation))
+	#get_tree().paused = true;
 	GameManager.transition_to_scene(target_level_path)
-	LevelTransitions.play_enter_transition(play_enter_animation)
+	await(LevelTransitions.play_enter_transition(play_enter_animation))
+	LevelTransitions.reset_transition_animation()
+	get_tree().paused = false
+	#queue_free()
