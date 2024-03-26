62acf9b59239fa291cb9252936cd953c59e41ead
mbrydg <matthew.brydges@ucalgary.ca>
2024-03-25 18:16:21 -0600

    Add fall-through platform tile to tilemap and rummage spot scene. Add is_near_rummagable to raccoon.gd

--- levels/object/rummage_spot.gd
+++ levels/object/rummage_spot.gd
@@ -1,13 +1,16 @@
 extends Area2D
 
 @export var food_content : int = 0
+var exhausted : bool = false
 
 # is_near_rummagable : Dictionary should be a member of raccoon.gd
 func _on_body_entered(body):
-	if "is_near_rummagable" in body:
-		body.set("is_near_rummagable", {"value": food_content})
-
+	if "is_near_rummagable" in body and not exhausted:
+		body.set("is_near_rummagable", {"spot": self, "value": food_content})
 
 func _on_body_exited(body):
 	if "is_near_rummagable" in body:
 		body.set("is_near_rummagable", {})
+
+func rummage():
+	exhausted = true
